ea8494aabb609a8a7d8bf4461493a5945a66bf63
ebruispir <50908568+ebruispir@users.noreply.github.com>
2019-08-30 00:13:57 +0200

    Update server.js

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,7 +9,26 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    if (request.url === '/state') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ state }));
+    } else if (request.url === '/add') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      state++;
+      response.write(JSON.stringify({ state }));
+    } else if (request.url === '/subtract') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      state--;
+      response.write(JSON.stringify({ state }));
+    } else if (request.url === '/reset') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      state = 10;
+      response.write(JSON.stringify({ state }));
+    } else {
+      response.writeHead(404, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ error: 'Not found' }));
+    }
+    response.end();
   });
 
   return server;
