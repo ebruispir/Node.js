7bcf0aae6076b2e782cb021a2e837305cde823b8
ebruispir <50908568+ebruispir@users.noreply.github.com>
2019-08-30 17:03:14 +0200

    Update server.js

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -5,31 +5,38 @@ const http = require('http');
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
+const sendResponse = (status, response, state) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(state));
+};
+
 function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    if (request.url === '/state') {
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      response.write(JSON.stringify({ state }));
-    } else if (request.url === '/add') {
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      state++;
-      response.write(JSON.stringify({ state }));
-    } else if (request.url === '/subtract') {
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      state--;
-      response.write(JSON.stringify({ state }));
-    } else if (request.url === '/reset') {
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      state = 10;
-      response.write(JSON.stringify({ state }));
-    } else {
-      response.writeHead(404, { 'Content-Type': 'application/json' });
-      response.write(JSON.stringify({ error: 'Not found' }));
+    switch (request.url) {
+      case '/state':
+        sendResponse(200, response, { state });
+        break;
+      case '/add':
+        state++;
+        sendResponse(200, response, { state });
+        break;
+      case '/subtract':
+        state--;
+        sendResponse(200, response, { state });
+        break;
+      case '/reset':
+        state = 10;
+        sendResponse(200, response, { state });
+        break;
+      default:
+        sendResponse(404, response, { error: 'Not found' });
+        break;
     }
     response.end();
   });
+
 
   return server;
 }
